3556a7d099d5711a15785bf62b5619dc8e144706
Geovane <geovanesilva.nar@hotmail.com>
2023-12-25 19:22:15 -0300

    Feat: Crud Client Controler.

--- src/main/java/com/example/cubochallenger/controller/ClientController.java
+++ src/main/java/com/example/cubochallenger/controller/ClientController.java
@@ -1,5 +1,47 @@
 package com.example.cubochallenger.controller;
 
+import com.example.cubochallenger.model.Client;
+import com.example.cubochallenger.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/clients")
 public class ClientController {
 
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> listarClient() {
+        List<Client> clients = clientService.buscarClients();
+        return ResponseEntity.ok(clients);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Client> buscarClient(@PathVariable("id") Long id) {
+        Client client = clientService.buscarClient(id);
+        return ResponseEntity.ok(client);
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> salvarClient(@RequestBody Client client) {
+        Client clientSalvo = clientService.salvarClient(client);
+        return ResponseEntity.ok(clientSalvo);
+    }
+
+    @PutMapping(path = "/{id}")
+   public ResponseEntity<Client> atualizarClient(@PathVariable Long id, @RequestBody Client client) {
+        clientService.atualizarClient(id, client);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletarClient(@PathVariable Long id) {
+        clientService.deletarClient(id);
+        return ResponseEntity.ok().build();
+    }
 }
