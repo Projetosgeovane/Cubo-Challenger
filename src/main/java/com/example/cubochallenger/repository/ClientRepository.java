94579802c952dd0c793c5c2e6b238e1130264d3d
Geovane <geovanesilva.nar@hotmail.com>
2023-12-25 19:22:32 -0300

    Feat: Crud Client Repository.

--- src/main/java/com/example/cubochallenger/repository/ClientRepository.java
+++ src/main/java/com/example/cubochallenger/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.example.cubochallenger.repository;
 
 import com.example.cubochallenger.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
